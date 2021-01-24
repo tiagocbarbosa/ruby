2788c0d664953db0047c35e832c335663fc4ec86
Tiago Castro Barbosa <tiago.c.barbosa@ericssoninovacao.com.br>
2021-01-23 21:19:46 -0300

    First commit

---
+++ program/pruby.rb
@@ -0,0 +1,73 @@
+=begin
+    BASICS
+=end
+
+puts 'Hello World'
+integer = 3
+float = 1.7
+string = 'Random text'
+puts 'Tell me your name:'
+STDOUT.flush
+your_name = gets.chomp
+puts 'Welcome ' + your_name
+
+=begin
+    VARIABLES AND CONSTANT
+=end
+
+# Local variable
+language = 'Ruby'
+
+# Instance variable
+@bank_account = nil
+
+# Class variable
+@@sign = 'lion'
+
+# Global variable
+$counter = 0
+
+# Constant
+Pi = 3.1416
+
+=begin
+    METHODS
+=end
+
+# Simple method
+def hello
+    'Hello'
+end
+# Using the method
+puts hello # -> Hello
+
+# Method with 1 argument
+def hello1 name
+    "Hello #{name}"
+end
+puts hello1 'Tiago' # -> Hello Tiago
+
+# Method with 2 arguments
+def hello2 (name, surname)
+    "Hello #{name} #{surname}"
+end
+puts hello2('Tiago', 'Barbosa') # -> Hello Tiago Barbosa
+
+# Method with default value
+def print_language best_language = 'C'
+    "#{best_language}"
+end
+puts print_language = 'Ruby' # -> Ruby
+
+# Aliasing a method
+def oldmtd
+    "Old method"
+end
+alias newmtd oldmtd
+def oldmtd
+    "Old improved method"
+end
+puts newmtd
+puts oldmtd
+
+# TO BE CONTINUED...
